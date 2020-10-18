bf9943dc31f30663be1a191d88b16ebb432cf47d
Honeysalad <oliveira.douglasedu@outlook.com>
2020-10-18 01:53:27 -0300

    shallow block generation

--- src/Timeline.js
+++ src/Timeline.js
@@ -4,6 +4,17 @@ import './Timeline.css';
 function Timeline() {
   return (
     <div className="timeline">
+      {
+        new Array(6).fill().map((item, i) => {
+          return (
+            <div className="line" key={`line ${i}`}>
+              {new Array(12).fill().map((item, j) => {
+                return <div className="version" key={`v${i}${j}`}/>
+              })}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
