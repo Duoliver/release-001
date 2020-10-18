90a311c646407e9c96873db0f88c6fc7ee9f6c2d
Honeysalad <oliveira.douglasedu@outlook.com>
2020-10-18 18:58:46 -0300

    do the (basic) evolution

--- src/Timeline.js
+++ src/Timeline.js
@@ -9,7 +9,23 @@ function Timeline() {
           return (
             <div className="line" key={`line ${i}`}>
               {new Array(12).fill().map((item, j) => {
-                return <div className="version" key={`v${i}${j}`}/>
+                const margin = j * 2
+                return (
+                  <div
+                    className="version-container"
+                    key={`vc${i}${j}`}
+                  >
+                    <div 
+                      className="version"
+                      style={{
+                        margin: `${margin}px ${margin}px ${margin}px 0`,
+                        height: `${50 - (j - 1) * 2}px`,
+                        borderRadius: `${j * 5}%`
+                      }}
+                      key={`v${i}${j}`}
+                    />
+                  </div>
+                )
               })}
             </div>
           )
