40bd7e8916ce27a16befc8ddf36b6d755dd5f4f9
Honeysalad <oliveira.douglasedu@outlook.com>
2020-10-18 19:57:03 -0300

    colour progression

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import './Timeline.css';
+import React from 'react'
+import './Timeline.css'
 
 function Timeline() {
+  
+  const magenta = [255, 0, 255]
+  const xCount = 15
+  const yCount = 9
+  const maxVersionSize = 60
+
   return (
     <div className="timeline">
       {
-        new Array(6).fill().map((item, i) => {
+        new Array(yCount).fill().map((item, i) => {
           return (
-            <div className="line" key={`line ${i}`}>
-              {new Array(12).fill().map((item, j) => {
+            <div className="line" key={`line ${i}`} style={{height: `${maxVersionSize}px`}}>
+              {new Array(xCount).fill().map((item, j) => {
                 const margin = j * 2
                 return (
                   <div
                     className="version-container"
+                    style={{
+                      height: `${maxVersionSize}px`,
+                      width: `${maxVersionSize}px`,
+                    }}
                     key={`vc${i}${j}`}
                   >
                     <div 
                       className="version"
                       style={{
                         margin: `${margin}px ${margin}px ${margin}px 0`,
-                        height: `${50 - (j - 1) * 2}px`,
-                        borderRadius: `${j * 5}%`
+                        height: `${maxVersionSize - (j - 1) * 2}px`,
+                        width: `${maxVersionSize}px`,
+                        borderRadius: `${j * 5}%`,
+                        backgroundColor: `rgb(
+                          ${magenta[0]*(j / xCount)},
+                          ${magenta[1]*(j / xCount)},
+                          ${magenta[2]*(j / xCount)}
+                        )`
                       }}
                       key={`v${i}${j}`}
                     />
